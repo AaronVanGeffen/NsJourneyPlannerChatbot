22f4cd20d9e96649dea0a4109e3646f59ef59275
Aaron van Geffen <aaron@aaronweb.net>
2017-03-02 18:34:34 +0100

    Initial commit of the NS API class.

---
+++ NsApi.py
@@ -0,0 +1,82 @@
+import json
+import re
+
+from datetime import datetime
+from urllib import request
+from xml.etree import ElementTree
+
+
+class NsApi:
+    ns_api_baseurl = "http://webservices.ns.nl/"
+
+    def __init__(self, login, password):
+        self.password_mgr = request.HTTPPasswordMgrWithDefaultRealm()
+        self.password_mgr.add_password(None, self.ns_api_baseurl, login, password)
+
+        self.handler = request.HTTPBasicAuthHandler(self.password_mgr)
+        self.opener = request.build_opener(self.handler)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.session.close()
+
+    def fetch(self, url):
+        return self.opener.open(self.ns_api_baseurl + url)
+
+    def getStationsAsList(self):
+        # http://webservices.ns.nl/ns-api-stations (English)
+        # http://webservices.ns.nl/ns-api-stations-v2 (Dutch, but includes more aliases)
+        response = self.fetch("ns-api-stations-v2")
+        if (response.status != 200):
+            raise("Could not fetch the list of stations.")
+
+        response = response.read().decode('utf-8')
+        tree = ElementTree.fromstring(response)
+
+        stations = [];
+        for station in tree:
+            stations.append(station.find("Code").text)
+
+            for name in [station.text for station in station.find("Namen")]:
+                stations.append(name)
+
+            for name in [station.text for station in station.find("Synoniemen")]:
+                stations.append(name)
+
+        stations.sort()
+        return stations
+
+
+    def getPossibleRoutes(self, fromStation, toStation, dateTime = datetime.now(), departureTime = True):
+        params = "fromStation={0}&toStation={1}&dateTime={2}&departure={3}".format(fromStation, toStation,
+            dateTime.strftime("%Y-%m-%dT%H:%M:%S"), departureTime)
+
+        response = self.fetch("ns-api-treinplanner?" + params)
+        if (response.status != 200):
+            raise("Could not fetch a list of potential routes.")
+
+        response = response.read().decode('utf-8')
+        tree = ElementTree.fromstring(response)
+
+        nowTime = datetime.now()
+
+        for route in tree:
+            departureTime = route.find("GeplandeVertrekTijd")
+            departureTime = datetime.strptime(departureTime.text, "%Y-%m-%dT%H:%M:%S%z")
+            if (departureTime - nowTime > 0):
+                break;
+
+        print (route)
+
+
+    def getJourneyPrice(self, fromStation, toStation):
+        params = "fromStation={0}&toStation={1}".format(fromStation, toStation)
+
+        response = self.fetch("ns-api-prijzen-v3?" + params)
+        if (response.status != 200):
+            raise("Could not fetch the price for this journey.")
+
+        raise("Not implemented")
+
