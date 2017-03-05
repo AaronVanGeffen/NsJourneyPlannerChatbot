12c425799116f4a1798b4f6c58054308d545d912
Aaron van Geffen <aaron@aaronweb.net>
2017-03-05 21:32:12 +0100

    Accept stations on a rule-based basis, allowing flexible entry.

--- clibot.py
+++ clibot.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+from enum  import Enum
 from NsApi import NsApi
 
-API_LOGIN    = ""
-API_PASSWORD = ""
+import re
+
 
 class NsBot:
     def __init__(self, login, password):
         self.ns = NsApi(login, password)
 
-        self.knownStations = self.ns.getStationsAsList()
-        self.knownStations = [x.lower() for x in self.knownStations]
+        self.knownStations      = self.ns.getStationsAsList()
+        self.knownStationsLower = [x.lower() for x in self.knownStations]
 
+        self.reReplyDeptStation  = re.compile("(van(?:uit)?|from)\s(?P<fromStation>['\-A-z]+)")
+        self.reReplyDestStation  = re.compile("(naar|to)\s(?P<toStation>['\-A-z]+)")
+        self.rePlanJourney1 = re.compile("(?:van(?:uit)?|from)\s(?P<fromStation>['\-A-z]+)\s(?:naar|to)\s(?P<toStation>['\-A-z ]+)")
+        self.rePlanJourney2 = re.compile("(?:naar|to)?\s(?P<toStation>['\-A-z]+)\s(?:van(?:uit)?|from)\s(?P<fromStation>['\-A-z ]+)")
 
 
     def compareList(self, list1, list2):
@@ -20,28 +25,90 @@ class NsBot:
         return False
 
 
+    def getStationInfoFromMsg(self, msg):
+        matches = self.rePlanJourney1.match(msg)
+        if matches == None:
+           matches = self.rePlanJourney2.match(msg)
+
+        if matches != None:
+            stations = {
+                'departure': matches.group('fromStation'),
+                'destination': matches.group('toStation')
+            }
+
+        else:
+            matches = self.reReplyDeptStation.match(msg)
+            if matches != None:
+                stations = {
+                    'departure': matches.group('fromStation')
+                }
+
+            else:
+                matches = self.reReplyDestStation.match(msg)
+                if matches != None:
+                    stations = {
+                        'destination': matches.group('toStation')
+                    }
+
+                else:
+                    stations = {}
+
+        print (stations)
+        return stations
+
+
+    def validateStations(self, stations):
+        valid = {}
+        for what in ['departure', 'destination']:
+            if what in stations:
+                station = stations[what].lower()
+                try:
+                    index = self.knownStationsLower.index(station)
+                    valid[what] = self.knownStations[index]
+                except ValueError:
+                    continue
+
+        print (valid)
+        return valid
+
+
+    def resetMemory(self):
+        self.memory = {}
+
+
+    def commitToMemory(self, stuff):
+        self.memory.update(stuff)
+
+
     def chat(self):
         planningList = ["plan", "planning"]
         quitList = ["quit", "stop"]
         confirmList = ["yes", "yeah", "yep", "sure", "y"]
         denyList = ["no", "nope", "n"]
 
-        print ("Hello! I am the NS-chatbot!")
-        print ("How can I help you?")
+        print ("Hello! Welcome to NS, the Dutch Railways! 🚃🚃🚃")
+        print ("Where would you like to go today?")
+
+        self.resetMemory()
 
         while True:
             userInput = input(">>> ")
             input_list = userInput.split()
 
+            # TODO: only accept these in certain chat state
+            stations = self.getStationInfoFromMsg(userInput)
+            stations = self.validateStations(stations)
+            self.commitToMemory(stations)
+
             if self.compareList(input_list, planningList):
                 print("From which station do you want to take the train?")
                 departureStation = input(">>> ")
-                if self.compareList(departureStation.lower().split(), self.knownStations):
+                if self.compareList(departureStation.lower().split(), self.knownStationsLower):
                     print("Valid station")
 
                 print("Where do you want to go?")
                 arrivalStation = input(">>> ")
-                if self.compareList(arrivalStation.lower().split(), self.knownStations):
+                if self.compareList(arrivalStation.lower().split(), self.knownStationsLower):
                     print("Valid station")
 
                 departureTime, travelTime = self.ns.getPossibleRoutes(departureStation, arrivalStation)
@@ -58,8 +125,8 @@ class NsBot:
 
 
 
-        if ns.compareList(input_list, quitList):
-                sys.exit("Goodbye!")
+API_LOGIN    = ""
+API_PASSWORD = ""
 
 
 ns = NsBot(API_LOGIN, API_PASSWORD)
